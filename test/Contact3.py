5c08987b50967e380e2abf29c08d5fa4c46dbe30
chupina.anastasia <chupin@ngs.ru>
2018-08-27 11:47:49 +0700

    login/out

--- test/Contact3.py
+++ test/Contact3.py
@@ -12,9 +12,8 @@ def app(request):
 
 
 def test_Contact3(app):
-    app.open_HP()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.open_cont_page()
     app.add_cont(Contacts("Святополк", "Владимирович", "Рюрик", "Окаянный", "Князь киевский", "Киевская Русь", "Киев кремль", "Кремль", "(123) 456-78-90", "Князь Тартурский", "(123) 456-78-90", "kiev@gmail.ua1", "kremlin@gmail.ua", "", "https://ru.wikipedia.org/wiki/вятополк Владимирович Окаянный", "", "", "был под арестом"))
-    app.logout()
+    app.session.logout()
 
