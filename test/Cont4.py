e52f68020280b4f0d69eea680dcbdfabc8166581
chupina.anastasia <chupin@ngs.ru>
2018-08-28 18:26:58 +0700

    request.addfinalizer(fixture.stop)

--- test/Cont4.py
+++ test/Cont4.py
@@ -12,7 +12,6 @@ def app(request):
 
 
 def test_Cont4(app):
-    #app.open_HP()
     app.session.login(username="admin", password="secret")
     app.contact.add(Contacts("Свят", "Влад-ч", "Рюрик1", "Окаянный1", "Князь 1", "Киевская1", "Кремль", "Кремль1", "(231) 456-78-90", "Князь Т", "(213) 456-78-90", "kiev1@gmail.ua1", "kremlin1@gmail.ua", "", "https://ru.wikipedia.org/wiki/вятополк Владимирович Окаянный", "", "", "был под арестом,бежал"))
     app.session.logout()
