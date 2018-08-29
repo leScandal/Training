6d5d5b1e89e008b20c3265ffc36e4e0dd4cffdc9
chupina.anastasia <chupin@ngs.ru>
2018-08-29 17:50:30 +0700

    общая фикстура

--- test/Cont4.py
+++ test/Cont4.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.Contacts import Contacts
-from fixture.application import Application
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.stop)
-    return fixture
 
 
 def test_Cont4(app):
