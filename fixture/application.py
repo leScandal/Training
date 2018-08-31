b3b17953544a203e90bbc2739a3a80b5ca700114
chupina.anastasia <chupin@ngs.ru>
2018-08-31 14:27:53 +0700

    application c мал.буквы

---
+++ fixture/application.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+from fixture.session import SessionHelper
+from fixture.contact import ContactHelper
+from fixture.group import GroupHelper
+
+class Application:
+   def __init__ (self):
+       self.wd = WebDriver()
+       self.wd.implicitly_wait(61)
+       self.session = SessionHelper(self)
+       self.contact = ContactHelper(self)
+       self.group = GroupHelper(self)
+
+   def open_HP(self):
+       wd = self.wd
+       wd.get("http://localhost/addressbook/")
+
+
+   def stop(self):
+       self.wd.quit()
