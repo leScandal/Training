04b848a3dc63cacf64c7dd266b8ef4896c67ef8e
chupina.anastasia <chupin@ngs.ru>
2018-09-04 17:00:28 +0700

    None в Group

--- fixture/group.py
+++ fixture/group.py
@@ -11,28 +11,47 @@ class GroupHelper:
         wd = self.app.wd
         self.open_GP()
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(Group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(Group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(Group.footer)
+        self.fill_group_form(Group)
         wd.find_element_by_name("submit").click()
         self.return_to_GP()
+
+
+    def change_name_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+
+    def fill_group_form(self, Group):
+        wd = self.app.wd
+        self.change_name_value("group_name", Group.name)
+        self.change_name_value("group_header", Group.header)
+        self.change_name_value("group_footer", Group.footer)
 
 
     def delete_first_group(self):
         wd = self.app.wd
         self.open_GP()
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group(wd)
         wd.find_element_by_name("delete").click()
         self.return_to_GP()
+
+    def modify_first(self, new_group_data):
+        wd = self.app.wd
+        self.open_GP()
+        self.select_first_group(wd)
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_data)
+        wd.find_element_by_name("update").click()
+        self.return_to_GP()
+
+    def select_first_group(self, wd):
+        wd.find_element_by_name("selected[]").click()
 
     def return_to_GP(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
         if not wd.find_element_by_name("selected[]").is_selected():
-             wd.find_element_by_name("selected[]").click()
+            self.select_first_group(wd)
