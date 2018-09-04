546db6b1474cf135b7410b705266c93e881bc43b
chupina.anastasia <chupin@ngs.ru>
2018-09-04 18:04:33 +0700

    try mpdify_gr

---
+++ test/test_modify_group.py
@@ -0,0 +1,14 @@
+from test.model.Group import Group
+
+
+def test_modify_name_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.Modify_first(Group(name="New group"))
+    app.session.logout()
+
+
+def test_modify_name_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.Modify_first(Group(header="New header"))
+    app.session.logout()
+
