1d43bcbc24ae25b0622978d2c84e7011a48b0726
chupina.anastasia <chupin@ngs.ru>
2018-09-06 12:25:45 +0700

    del contact, add group - работает, принимает третий атрибут. модификации работает

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,11 +4,11 @@ from model.Group import Group
 
 def test_modify_name_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.Modify_first(Group(name="New group", header="New header",footer="New header")
-    #app.session.logout()
+    app.group.Modify_first(Group(name="New group1", header="New header1",footer="New header1"))
+    app.session.logout()
 
-#
-# def test_modify_header_group(app):
-#     app.session.login(username="admin", password="secret")
-#     app.group.Modify_first(Group(None,header="New header",None))
-#     app.session.logout()
+
+def test_modify_header_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.Modify_first(Group(header="header"))
+    app.session.logout()
