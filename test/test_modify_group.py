8a738df0e1bdc263bde4bbece9533041d9b3a293
chupina.anastasia <chupin@ngs.ru>
2018-09-08 17:45:55 +0700

    проверка : создаёт перед удалением группы

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,6 +3,8 @@ from model.Group import Group
 
 
 def test_modify_name_group(app):
+    if app.group.count() == 0:
+        app.group.Create(Group(name="for del"))
     app.group.Modify_first(Group(name="123844"))
     app.session.logout()
 
