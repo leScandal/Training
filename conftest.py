1ff1861a93a8995d6c3fc8afe9826c66f57b7d61
chupina.anastasia <chupin@ngs.ru>
2018-10-25 13:52:53 +0700

    Задание 17, все работает, потерянный target восстановила

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 import pytest
 import json
-import os
+import os.path
 from fixture.application import Application
 
 fixture = None
@@ -12,8 +12,9 @@ def app(request):
     global target
     browser = request.config.getoption("--browser")
     if target is None:
-        with open(request.config.getoption("--target")) as config_file:
-            target = json.load(config_file)
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as con_file:
+            target = json.load(con_file)
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url = target['baseUrl'])
     fixture.session.ensure_login(username = target['username'], password = target['password'])
@@ -30,4 +31,5 @@ def ostan(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default = "firefox")
+    parser.addoption("--target", action="store", default="target.json")
     #f
