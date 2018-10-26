590597da78f998b192977e844465421f0c342e49
chupina.anastasia <chupin@ngs.ru>
2018-10-26 14:56:28 +0700

    Задание 19 contacts, grpoup с пакетом jsonpickle

--- generator/group.py
+++ generator/group.py
@@ -3,7 +3,7 @@ from model.group import Group
 import random
 import string
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -41,5 +41,11 @@ testdata = [Group(name="", header="", footer="")]  + [
 # ]
 
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
 with open(file, "w") as out_file:
-    out_file.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json",  indent=2)
+    out_file.write(jsonpickle.encode(testdata))
+
+# Для библиотеки json
+# with open(file, "w") as out_file:
+#     out_file.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
