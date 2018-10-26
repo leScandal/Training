ee8b121511c15c17fab1d55a8535e1e1ba04dfa7
chupina.anastasia <chupin@ngs.ru>
2018-10-26 16:37:56 +0700

    Задание 19, ничего не происходит и ошибка: Process finished with exit code 0

---
+++ data/add_const_gr.py
@@ -0,0 +1,7 @@
+from model.group import Group
+
+
+constant = [
+    Group("asd", "sdf 2", "qwerty 2"),
+    Group("asd3", "sdf 3", "qwerty 3")
+]
---
+++ test/test_add_gr.py
@@ -0,0 +1,17 @@
+from model.group import Group
+import pytest
+#from data.add_const_gr import constant as testdata
+from generator.group import testdata
+
+
+#@pytest.mark.parametrize("data", testdata, ids=[repr(x) for x in testdata])
+def test_Add_Group(app, json_groups):
+        group = json_groups
+        old_groups = app.group.get_group_list()
+        app.group.Create(group)
+        assert len(old_groups) + 1 == app.group.count()
+        new_groups = app.group.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+
