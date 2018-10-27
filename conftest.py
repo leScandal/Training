3aa03e7c7619b1b395723be5d878f6e62c304815
chupina.anastasia <chupin@ngs.ru>
2018-10-27 21:14:17 +0700

    Задание 20, database, тестs крутит, но не исполняет

--- conftest.py
+++ conftest.py
@@ -4,24 +4,43 @@ import os.path
 import importlib
 import jsonpickle
 from fixture.application import Application
+from fixture.db import DbFixture
 
 
 fixture = None
 target = None
 
+
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+#       config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as con_file:
+            target = json.load(con_file)
+    return target
+
+
 @pytest.fixture
 def app(request):
     global fixture
-    global target
     browser = request.config.getoption("--browser")
-    if target is None:
-        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
-        with open(config_file) as con_file:
-            target = json.load(con_file)
+    web_config = load_config(request.config.getoption("--target"))['web']
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url = target['baseUrl'])
-    fixture.session.ensure_login(username = target['username'], password = target['password'])
+        fixture = Application(browser=browser, base_url = web_config['baseUrl'])
+    fixture.session.ensure_login(username = web_config['username'], password = web_config['password'])
     return fixture
+
+
+@pytest.fixture(scope="session")
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))['db']
+    dbfixture = DbFixture(host = db_config['host'], database = db_config['database'], user = db_config['user'], password = db_config['password'])
+    def fin():
+        dbfixture.stop()
+    request.addfinalizer(fin)
+    return dbfixture
+
 
 @pytest.fixture(scope = "session", autouse=True)
 def ostan(request):
@@ -39,10 +58,10 @@ def pytest_addoption(parser):
 
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
-        if fixture .startswith ("data_"):
+        if fixture.startswith ("data_"):
             testdata = load_from_module(fixture[5:])
             metafunc.parametrize(fixture, testdata, ids = [str (x) for x in testdata])
-        elif fixture .startswith ("json_"):
+        elif fixture.startswith ("json_"):
             testdata = load_from_json(fixture[5:])
             metafunc.parametrize(fixture, testdata, ids = [str (x) for x in testdata])
 
