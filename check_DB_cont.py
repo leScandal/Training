e109804246ea55cee1a086eed972b92f201cca12
chupina.anastasia <chupin@ngs.ru>
2018-11-04 18:19:39 +0700

    Задание 21, проверка соединения с БД на ктнтактах

---
+++ check_DB_cont.py
@@ -0,0 +1,12 @@
+from fixture.db import DbFixture
+
+db = DbFixture (host="127.0.0.1", database = "addressbook", user = "root", password = "")
+#connection = mysql.connector.connect(host="127.0.0.1", database = "addressbook", user = "root", password = "")
+
+try:
+    contacts = db.get_cont_list()
+    for contact in contacts:
+        print(contact)
+    print(len(contacts))
+finally:
+    db.stop()
