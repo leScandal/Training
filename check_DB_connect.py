9ef2a2334ae9dfa92e0c7aedcaa52ddd93e3f700
chupina.anastasia <chupin@ngs.ru>
2018-11-04 14:39:30 +0700

    Задание 21, check UI vs DB contacts & droups

--- check_DB_connect.py
+++ check_DB_connect.py
@@ -2,7 +2,7 @@
 import pymysql
 
 connection = pymysql.connect(host="127.0.0.1", database = "addressbook", user = "root", password = "")
-#connection = mysql.connector .connect(host="127.0.0.1", database = "addressbook", user = "root", password = "")
+#connection = mysql.connector.connect(host="127.0.0.1", database = "addressbook", user = "root", password = "")
 
 try:
     cursor = connection.cursor()
