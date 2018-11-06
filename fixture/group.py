80ec5fe8107f521ef60288211161dbef93fedc5e
chupina.anastasia <chupin@ngs.ru>
2018-11-06 12:09:29 +0700

    Задание 20, удаление из БД на группах, работает

--- fixture/group.py
+++ fixture/group.py
@@ -43,6 +43,15 @@ class GroupHelper:
         self.group_cache = None
 
 
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_GP()
+        self.select_group_by_id(id)
+        wd.find_element_by_name("delete").click()
+        self.return_to_GP()
+        self.group_cache = None
+
+
     def delete_first_group(self):
         self.delete_group_by_index(0)
 
@@ -66,9 +75,16 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
+
     def select_group_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
+
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value = '%s']" %id).click()
+
 
     def return_to_GP(self):
         wd = self.app.wd
---
+++ test/Task20/test_del_group20.py
@@ -0,0 +1,18 @@
+from model.group import Group
+import random
+import pytest
+
+def test_Del_some_Group(app, db):
+    if len(db.get_group_list()) == 0:
+        app.group.Create(Group(name="for del"))
+    old_groups = db.get_group_list()
+    group = random.choice(old_groups)
+    #index = randrange (len(old_groups))
+    app.group.delete_group_by_id(group.id)
+    #assert len(old_groups) - 1 == app.group.count()
+    new_groups = db.get_group_list()
+    old_groups.remove(group)
+    assert old_groups == new_groups
+
+
+
