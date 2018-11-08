a986c05d47036a56028e91b873f06f6e499530c2
chupina.anastasia <chupin@ngs.ru>
2018-11-08 19:17:18 +0700

    Задание 22 , check orm connection

---
+++ check_DB_ORM.py
@@ -0,0 +1,13 @@
+import mysql.connector
+from fixture.orm import ORMFixture
+
+db = ORMFixture(host="127.0.0.1", database = "addressbook", user = "root", password = "")
+#connection = mysql.connector.connect(host="127.0.0.1", database = "addressbook", user = "root", password = "")
+
+try:
+    l = db.get_group_list()
+    for item in l:
+        print(item)
+    print(len(l))
+finally:
+    pass #db.stop()
---
+++ fixture/orm.py
@@ -0,0 +1,31 @@
+from pony.orm import *
+from datetime import datetime
+
+
+class ORMFixture:
+    db = Database()
+
+    class ORMGroup(db.Entity):
+        _table_ = "gorup_list"
+        id = PrimaryKey(int, column = 'group_id')
+        name = Optional (str, column = 'group_name')
+        header = Optional(str, column='group_header')
+        footer = Optional(str, column='group_footer')
+
+
+
+    class ORMContact(db.Entity):
+        _table_ = "addressbook"
+        id = PrimaryKey(int, column = 'id')
+        firstname = Optional (str, column = 'firstname')
+        lastname = Optional(str, column='lastname')
+        deprecated = Optional(datetime, column='deprecated')
+
+
+    def __init__(self, host, database, user, password):
+        self.db.bind('mysql', host=host, database=database, user=user, password=password)
+        self.db.generate_mappping()
+
+    @db_session
+    def get_group_list(self):
+        return list(select(g for g in ORMFixture.ORMGroup))
