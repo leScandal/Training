08e0e76b9d9801975b33a0ff7804ab44ae0ae9b5
chupina.anastasia <chupin@ngs.ru>
2018-11-09 15:18:05 +0700

    Задание 22 , check orm cont не проходит

--- fixture/orm.py
+++ fixture/orm.py
@@ -1,6 +1,8 @@
 from pony.orm import *
 from datetime import datetime
-
+from model.group import Group
+from model.contacts import Contacts
+from pymysql.converters import  decoders
 
 class ORMFixture:
     db = Database()
@@ -14,7 +16,7 @@ class ORMFixture:
 
 
 
-    class ORMContact(db.Entity):
+    class ORMContacts(db.Entity):
         _table_ = "addressbook"
         id = PrimaryKey(int, column = 'id')
         firstname = Optional (str, column = 'firstname')
@@ -23,9 +25,28 @@ class ORMFixture:
 
 
     def __init__(self, host, database, user, password):
-        self.db.bind('mysql', host=host, database=database, user=user, password=password)
+        self.db.bind('mysql', host=host, database=database, user=user, password=password, conv=decoders)
         self.db.generate_mapping()
+        sql_debug(True)
+
+    def convert_groups_to_model(self, groups):
+        def convert(group):
+            return Group (id = str (group.id), name = group.name, header = group.header, footer = group.footer)
+        return list(map(convert, groups))
+
+    def convert_contacts_to_model(self, contacts):
+        def convert(contact):
+            return Contacts(id = str (contact.id), name = contact.firstname, lastN = contact.lastname) #, footer = contact.footer)
+        return list(map(convert, contacts))
+
+    # cursor.execute(
+    #     "select id, firstname, middlename, lastname, company, title, address, home, mobile, work, fax, email, email2, email3, homepage, notes from addressbook where deprecated='0000-00-00 00:00:00'")
+
 
     @db_session
     def get_group_list(self):
-        return list(select(g for g in ORMFixture.ORMGroup))
+        return self.convert_groups_to_model(select(g for g in ORMFixture.ORMGroup))
+
+    @db_session
+    def get_cont_list(self):
+        return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContacts if c.deprecated is None))
