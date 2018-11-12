6dce851f151c14069abbbee3ca1b1023b4cb28b1
chupina.anastasia <chupin@ngs.ru>
2018-11-12 17:09:51 +0700

    T22 del print

--- test/Task22/test_out_gr22.py
+++ test/Task22/test_out_gr22.py
@@ -15,34 +15,14 @@ def test_cont_out_gr(app, db, orm):
     app.group.view_group_id(list_groups[any_gr].id) # вход в случайно выбранную группу
     db_list_gr = orm.get_cont_in_gr(Group(id=idx)) #list_groups[any_gr].id)) #спсиок контактов в гр до
     if len(db_list_gr) == 0:
-        print("empty group")
-        print (idx)
         list_cont = db.get_cont_list()
         any_cont = randrange(len(list_cont))
         app.group.full_group_list()
         app.contact.into_group(any_cont, any_gr) #контакт в группу)
         app.contact.return_to_HP()
     ui_cont_gr2 = app.contact.get_cont_list1() #  список контактов в гр чз ПИ после добавления , если пустая
-    print(ui_cont_gr2[0])
-    print(ui_cont_gr2[0].id)
-    print("id контактa в группе")
-    print("group")
-    print("list cont  in gr after adding:")
-    print(ui_cont_gr2)
     app.contact.out_group(ui_cont_gr2[0].id, idx) # del 1й контaкт
     app.contact.return_to_HP()
     db_list_gr = orm.get_cont_in_gr(Group(id=list_groups[any_gr].id))
-    print(db_list_gr)
     assert Contacts(id=ui_cont_gr2[0].id) not in db_list_gr, "Error"
 
-
-#
-# def test_cont_out_gr114(app, db, orm):
-#     app.group.view_group_id(114) # вход в  группу 114 34cd
-#     ui_cont_gr = app.contact.get_cont_list1() #список контактов в гр чз ПИ
-#     app.contact.out_group(ui_cont_gr[0].id, 114) # del 1й контaкт
-#     app.contact.return_to_HP()
-#     app.group.view_group_id(114) # вход в случайно выбранную группу
-#     db_list_gr = orm.get_cont_in_gr(Group(id=114)) #новый список конт чз орм
-#     assert Contacts(id=ui_cont_gr[0].id) not in db_list_gr, "Error"
-#
