b76b9c767be43928c707cc484a3181661249ad50
chupina.anastasia <chupin@ngs.ru>
2018-11-14 15:12:15 +0700

    Task 21, разделила and на 2 append, ну уж точно не ошибка, оператор принт кажет списки

--- test/Task21/test_DB_vs_UI_cont.py
+++ test/Task21/test_DB_vs_UI_cont.py
@@ -18,9 +18,11 @@ def test_check_cont_lists (app, db):
     for item_db in db_list:
         all_phones_HP = merge_phones_like_HP(item_db)
         all_mails_HP = merge_mails_like_HP(item_db)
-        db_mails_tels.append(all_phones_HP and all_mails_HP)
+        db_mails_tels.append(all_phones_HP)
+        db_mails_tels.append(all_mails_HP)
     for item_ui in ui_list:
-        ui_mails_tels.append(item_ui.all_phones_from_HP and item_ui.all_mails_from_HP)
+        ui_mails_tels.append(item_ui.all_phones_from_HP)
+        ui_mails_tels.append(item_ui.all_mails_from_HP)
     assert sorted(ui_mails_tels) == sorted(db_mails_tels)
 
 
@@ -33,8 +35,8 @@ def merge_phones_like_HP(contact):
                              map(lambda x: clear(x),
                                  filter(lambda z:  z is not None,
                                         [contact.home, contact.mobile, contact.work, contact.phone2]))))
-
 def merge_mails_like_HP(contact):
+
     return "\n".join(filter (lambda y: y != "",
                              map(lambda x: clear(x),
                                  filter(lambda z:  z is not None,
